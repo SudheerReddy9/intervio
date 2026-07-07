d1a872fc772382eb4ab3cf1b391d5dbd390844a4
Sudheer Reddy <pokalasudheer143@gmail.com>
2026-07-06 21:05:32 -0400

    Create application color palette

---
+++ src/theme/palette.ts
@@ -0,0 +1,47 @@
+import { PaletteOptions } from '@mui/material/styles';
+
+export const lightPalette: PaletteOptions = {
+    mode: 'light',
+    // Brand Color
+    primary: {
+        main: '#4F46E5', //Indgo 600
+        light: '#6366F1',
+        dark: '#4338CA',
+        contrastText: '#FFFFFF',
+    },
+
+    secondary: {
+        main: '#06B6D4',
+        light: '#22D3EE',
+        dark: '#0891B2',
+        contrastText: '#FFFFFF',
+    },
+    // Status Colors
+    success: {
+        main: '#16A34A',
+    },
+
+    warning: {
+        main: '#F59E0B',
+    },
+
+    error: {
+        main: '#DC2626',
+    },
+
+    info: {
+        main: '#0284C7',
+    },
+    // Layout Colors
+    background: {
+        default: '#F8FAFC',
+        paper: '#FFFFFF',
+    },
+    // Typography Colors
+    text: {
+        primary: '#0F172A',
+        secondary: '#64748B',
+    },
+    //Borders
+    divider: '#E2E8F0',
+};
