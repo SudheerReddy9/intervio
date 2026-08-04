1a3e0e52e33f79a5c9e482d3e2f4235ef55f47de
Sudheer Reddy <pokalasudheer143@gmail.com>
2026-08-04 09:08:43 -0400

    Updated the code for interview

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,12 +1,37 @@
+'use client';
 import QuestionCard from "@/features/speech/components/QuestionCard";
 import SpeechRecorder from "@/features/speech/components/SpeechRecorder";
-import { Container } from "@mui/material";
+import { resumeQuestions } from "@/features/speech/data/resumeQuestion";
+import { Box, Button, Container } from "@mui/material";
+import { useState } from "react";
 
 export default function InterviewPage() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
     return (
         <Container>
-            <QuestionCard />
+            <QuestionCard
+                question={resumeQuestions[currentQuestion].question}
+                questionNumber={currentQuestion + 1}
+                totalQuestions={resumeQuestions.length}
+            />
             <SpeechRecorder />
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    mt: 2,
+                }}
+            >
+                <Button
+                    onClick={() => {
+                        if (currentQuestion < resumeQuestions.length - 1) {
+                            setCurrentQuestion(currentQuestion + 1);
+                        }
+                    }}
+                >
+                    Next Question
+                </Button>
+            </Box>
         </Container>
     );
 }
